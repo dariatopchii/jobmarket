1bb9d1d540fa338847b3f9b881d6714010fe7bf7
Oleksandr Topchii <farexyg@gmail.com>
2022-06-24 19:32:48 +0300

    header log in

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -12,7 +12,35 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Main from '../Main/Main.js';
 
 class Header extends React.Component {
-  render() {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userLoggedIn: false
+        }
+    }
+    
+  componentDidMount() {
+      this.waitForUpdates();
+  }
+
+    async waitForUpdates(){
+      while (true){
+          await Header.sleep(1000);
+          if(localStorage.getItem('user')){
+              this.setState({
+                  userLoggedIn: true
+              })
+          }
+      }
+    }
+
+    static sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    render() {
     return(
       <div className='headerContainer'>
             <div className="logo">
@@ -23,14 +51,29 @@ class Header extends React.Component {
             </div>
             <div className="nav-item">    
               <Link to='/Cv' className="nav-link"><p>Резюме</p></Link>   
-            </div>    
-            <div className="nav-item">    
-              <Link to='/Login' className="nav-link"><p>Login</p></Link>   
-            </div> 
-            <div className="nav-item">    
-              <Link to='/Signup' className="nav-link"><p>Sign Up</p></Link>    
-              </div>        
- 
+            </div>
+          {
+              this.state.userLoggedIn
+              ? (<div className="nav-item">
+                      <Link to='/Logout' className="nav-link"><p>Logout</p></Link>
+                  </div>) 
+              : <></>
+          }
+          {
+              !this.state.userLoggedIn
+                  ? (<div className="nav-item">
+                      <Link to='/Login' className="nav-link"><p>Login</p></Link>
+                  </div>)
+                  : <></>
+          }
+          {
+              !this.state.userLoggedIn
+                  ? (<div className="nav-item">
+                      <Link to='/Signup' className="nav-link"><p>Sign Up</p></Link>
+                  </div>)
+                  : <></>
+
+          }
           {/* <Routes>  
             <Route path='/Dashboard' element={<Dashboard />} />    
           </Routes>     */}
